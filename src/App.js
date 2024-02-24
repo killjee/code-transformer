e348232e5f2b625a8dbc22f46212f64c5d57364d
Satyam Shubham <killjee.satyam@gmail.com>
2024-02-25 02:46:44 +0530

    Abstracted network layer, abstracted api call to /play endpoint

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import './App.css';
 import { Selectors } from './selector';
 import { Controls } from './controls';
 import { Track, TrackArgument } from './track';
+import { CallPlay } from './network';
 
 function App() {
   const [languages, setLanguages] = useState([]);
@@ -121,40 +122,8 @@ function App() {
   };
 
   const handlePlayButtonTap = async () => {
-    var trackArgs = {}
-    var track = {}
-
-    // Since track can have multiple kind of arguments
-    //    1. No argumens
-    //    2. Arguments with no value
-    //    3. Arguments with values
-    // This block prepares track for the request with current state of arguments
-    if (preparedTrack.argumentMap.length === 0) {
-      track = preparedTrack.key
-    } else if (preparedTrack.argumentMap.length === 1 && preparedTrack.argumentMap[0].value == null) {
-      track[preparedTrack.key] = preparedTrack.argumentMap[0].key
-    } else {
-      preparedTrack.argumentMap.forEach((arg) => {
-        trackArgs[arg.key] = arg.value
-      })
-      track[preparedTrack.key] = trackArgs
-    }
-
-    var requestBody = JSON.stringify({
-      source: sourceInput,
-      language: selectedLang,
-      track: track
-    })
-
-    // Set all state after Getting response
     setPlayButtonDisabled(true)
-    const response = await fetch('https://lyfkykbisow7zkharariyw26ia0zppmy.lambda-url.us-east-1.on.aws/play', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: requestBody
-    })
+    const response = await CallPlay(preparedTrack, sourceInput, selectedLang)
     setPlayButtonDisabled(false)
     setCodeGenerated(true)
 
